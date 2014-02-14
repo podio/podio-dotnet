5da2c0cebab415a20279867042f5802c978399f8
Ajmal Vh <avh@phases.dk>
2014-02-14 16:49:22 +0530

    Added an Interface OAuthStore, and implementation to store OAuth info in session.

---
+++ Podio .NET/Utils/Authentication/IAuthStore.cs
@@ -0,0 +1,22 @@
+﻿namespace PodioAPI.Utils
+{
+    public interface IAuthStore
+    {
+        /// <summary>
+        /// Get PodioOAuth object from store
+        /// </summary>
+        /// <returns></returns>
+        PodioOAuth Get();
+
+        /// <summary>
+        /// Store PodioOAuth object from store
+        /// </summary>
+        /// <param name="podioOAuth"></param>
+        void Set(PodioOAuth podioOAuth);
+       
+        /// <summary>
+        /// Remove PodioOAuth from store
+        /// </summary>
+        void Distroy();
+    }
+}
---
+++ Podio .NET/Utils/Authentication/SessionStore.cs
@@ -0,0 +1,28 @@
+﻿using System.Web;
+
+namespace PodioAPI.Utils
+{
+    /// <summary>
+    /// Store PodioOAuth info in session
+    /// </summary>
+    class SessionStore: IAuthStore
+    {
+        public PodioOAuth Get()
+        {
+            if (HttpContext.Current.Session["PodioOAuth"] != null)
+                return HttpContext.Current.Session["PodioOAuth"] as PodioOAuth;
+            else
+                return new PodioOAuth();
+        }
+
+        public void Set(PodioOAuth podioOAuth)
+        {
+            HttpContext.Current.Session["PodioOAuth"] = podioOAuth;
+        }
+
+        public void Distroy()
+        {
+            HttpContext.Current.Session["PodioOAuth"] = null;
+        }
+    }
+}
