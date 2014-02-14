79f0dac7947bc4898ee9067d59ab6bcfac11038b
Ajmal Vh <avh@phases.dk>
2014-02-14 16:50:50 +0530

    Added custom NestedDictionary converter.

--- Podio .NET/Utils/JSONSerialization/JSONSerializer.cs
+++ Podio .NET/Utils/JSONSerialization/JSONSerializer.cs
@@ -1,22 +1,48 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PodioAPI.Utils
 {
-    class JSONSerializer
+    internal class JSONSerializer
     {
-        internal static string Serilaize(object entity)
+        public static string Serilaize(object entity)
         {
             return JsonConvert.SerializeObject(entity);
         }
 
-        internal static object Deserilaize<T>(string json)
+        public static object Deserilaize<T>(string json)
         {
-            return JsonConvert.DeserializeObject<T>(json);
+            return JsonConvert.DeserializeObject<T>(json, new JsonConverter[] { new NestedDictionaryConverter() });
         }
     }
+
+    #region Nested Dictionary converter
+    internal class NestedDictionaryConverter : Newtonsoft.Json.Converters.CustomCreationConverter<IDictionary<string, object>>
+    {
+        public override IDictionary<string, object> Create(Type objectType)
+        {
+            return new Dictionary<string, object>();
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            // in addition to handling IDictionary<string, object>
+            // we want to handle the deserialization of dict value
+            // which is of type object
+            return objectType == typeof(object) || base.CanConvert(objectType);
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            if (reader.TokenType == JsonToken.StartObject
+                || reader.TokenType == JsonToken.Null)
+                return base.ReadJson(reader, objectType, existingValue, serializer);
+
+            // if the next token is not an object
+            // then fall back on standard deserializer (strings, numbers etc.)
+            return serializer.Deserialize(reader);
+        }
+    }
+    #endregion
 }
