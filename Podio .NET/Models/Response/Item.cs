6b29b3853cd898f89cb15382d0b8002e51c362eb
Ajmal Vh <avh@phases.dk>
2014-02-19 18:19:59 +0530

    Added empty item model.

---
+++ Podio .NET/Models/Response/Item.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PodioAPI.Models.Response
+{
+    /// <summary>
+    /// Not implemented yet
+    /// </summary>
+    public class Item
+    {
+
+    }
+}
