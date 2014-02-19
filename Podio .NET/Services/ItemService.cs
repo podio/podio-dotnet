97c97be05cac068463189a6c0db086a1531a23ab
Ajmal Vh <avh@phases.dk>
2014-02-19 18:24:14 +0530

    Added GetItemMethod to ItemService

--- Podio .NET/Services/ItemService.cs
+++ Podio .NET/Services/ItemService.cs
@@ -1,4 +1,9 @@
-﻿namespace PodioAPI.Services
+﻿using PodioAPI.Models.Response;
+using PodioAPI.Models.Request;
+using PodioAPI.Utils;
+using System.Collections.Generic;
+
+namespace PodioAPI.Services
 {
     public class ItemService
     {
@@ -11,13 +16,17 @@
         /// <summary> Returns the item with the specified id. 
         /// <para>Podio API Reference: https://developers.podio.com/doc/items/get-item-22360 </para>
         /// </summary> 
-        /// 
-        /* public Item Get(int itemId)
+        /// <param name="itemId"></param>
+        /// <param name="markedAsViewed">If true marks any new notifications on the given item as viewed, otherwise leaves any notifications untouched, Default value true</param>       
+        public Item GetItem(int itemId, bool markedAsViewed = true)
         {
-            //TODO   
+            var markAsViewdValue = markedAsViewed == true ? "1" : "0";
+            var attributes = new Dictionary<string, string>()
+            {
+                {"mark_as_viewed", markAsViewdValue}
+            };
+            var url = string.Format("/item/{0}", itemId);
+            return _podioInstance.Get<Item>(url, attributes);
         }
-        */
     }
-
-
 }
