2cfab6af0c4de63dadf035583d442a3535d8235b
Ajmal Vh <avh@phases.dk>
2014-03-05 10:38:35 +0530

    Added response models for FileResponse and ItemCalculate

---
+++ Podio .NET/Models/Response/FileResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PodioAPI.Models.Response
+{
+    public class FileResponse
+    {
+        public byte[] FileContents { get; set; }
+        public string ContentType { get; set; }
+        public long ContentLength { get; set; }
+    }
+}
---
+++ Podio .NET/Models/Response/ItemCalculate.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PodioAPI.Models.Response
+{
+   public class ItemCalculate
+    {
+       [JsonProperty("total")]
+       public double? Total { get; set; }
+
+       [JsonProperty("groups")]
+       List<CalculationGroup> CalculationGroups { get; set; }
+
+    }
+   public class CalculationGroup
+   {
+       [JsonProperty("groups")]
+       public List<ValueGroup> ValueGroups { get; set; }
+
+       [JsonProperty("count")]
+       public double? Count { get; set; }
+   }
+    public class ValueGroup
+    {
+        [JsonProperty("value")]
+        public string Value { get; set; }
+
+        [JsonProperty("label")]
+        public string Label { get; set; }
+    }
+}
