aff11fd3661f514b10aa41a8279d6c88fe1a378f
Ajmal Vh <avh@phases.dk>
2014-04-10 14:57:00 +0530

    Added space service

---
+++ Podio .NET/Services/SpaceService.cs
@@ -0,0 +1,16 @@
+﻿using PodioAPI.Models;
+using System.Collections.Generic;
+
+namespace PodioAPI.Services
+{
+    public class SpaceService
+    {
+        private Podio _podio;
+        public SpaceService(Podio currentInstance)
+        {
+            _podio = currentInstance;
+        }
+
+        
+    }
+}
