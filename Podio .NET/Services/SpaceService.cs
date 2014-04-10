2fd2b123ca4ca20f70e89fb9cc49e4bfdccef739
Ajmal Vh <avh@phases.dk>
2014-04-10 17:41:23 +0530

    added update space method

--- Podio .NET/Services/SpaceService.cs
+++ Podio .NET/Services/SpaceService.cs
@@ -37,5 +37,31 @@ namespace PodioAPI.Services
             dynamic respone = _podio.Post<dynamic>(url, requestData);
             return (int)respone["space_id"];
         }
+
+        /// <summary>
+        /// Updates the space with the given id.
+        /// <para>Podio API Reference: https://developers.podio.com/doc/spaces/update-space-22391 </para>
+        /// </summary>
+        /// <param name="spaceId"></param>
+        /// <param name="name">The name of the space</param>
+        /// <param name="urlLabel"> The new URL label, if any changes</param>
+        /// <param name="privacy">The privacy level of the space, either "open" or "closed", defaults to "closed"</param>
+        /// <param name="autoJoin">True if new employees should be joined automatically, false otherwise, defaults to false</param>
+        /// <param name="postOnNewApp">True if new apps should be announced with a status update, false otherwise</param>
+        /// <param name="postOnNewMember">True if new members should be announced with a status update, false otherwise</param>
+        public void UpdateSpace(int spaceId, string name = "", string urlLabel = null, string privacy = null, bool? autoJoin = null, bool? postOnNewApp = null, bool? postOnNewMember = null)
+        {
+            string url = string.Format("/space/{0}", spaceId);
+            dynamic requestData = new
+            {
+                name = name,
+                url_label = urlLabel,
+                privacy = privacy,
+                auto_join = autoJoin,
+                post_on_new_app = postOnNewApp,
+                post_on_new_member = postOnNewMember
+            };
+            _podio.Put<dynamic>(url, requestData);
+        }
     }
 }
