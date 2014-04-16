812355da92385520b6eaa9382938fec377dccd73
Ajmal Vh <avh@phases.dk>
2014-04-16 15:07:27 +0530

    More methods to space member service.

--- Podio .NET/Services/SpaceMembersService.cs
+++ Podio .NET/Services/SpaceMembersService.cs
@@ -34,7 +34,7 @@ namespace PodioAPI.Services
         /// <returns></returns>
         public SpaceMember GetSpaceMember(int spaceId, int userId)
         {
-            string url = string.Format("/space/{0}/member/{1}/v2", spaceId,userId);
+            string url = string.Format("/space/{0}/member/{1}/v2", spaceId, userId);
             return _podio.Get<SpaceMember>(url);
         }
 
@@ -49,6 +49,93 @@ namespace PodioAPI.Services
         {
             string url = string.Format("/space/{0}/member/{1}/", spaceId, role);
             return _podio.Get<List<SpaceMember>>(url);
+        }
+
+        /// <summary>
+        /// Returns the members of the given space.
+        /// <para>Podio API Reference: https://developers.podio.com/doc/space-members/get-space-members-v2-19350328 </para>
+        /// </summary>
+        /// <param name="spaceId"></param>
+        /// <param name="memberType">The type of members to return. Can be one of: employee, external, admin, regular, light, guest.</param>
+        /// <param name="query">Any search term to match.</param>
+        /// <param name="limit">The maximum number of members to return. Default value: 100</param>
+        /// <param name="offset">The offset into the member list. Default value: 0</param>
+        /// <returns></returns>
+        public List<SpaceMember> GetSpaceMembers(int spaceId, string memberType = null, string query = null, int limit = 100, int offset = 0)
+        {
+            string url = string.Format("/space/{0}/member/v2/", spaceId);
+            var requestData = new Dictionary<string, string>()
+            {
+                {"limit",limit.ToString()},
+                {"member_type",memberType},
+                {"offset",offset.ToString()},
+                {"query",query}
+            };
+            return _podio.Get<List<SpaceMember>>(url, requestData);
+        }
+
+        /// <summary>
+        /// Used to get the details of an active users membership of a space.
+        /// <para>Podio API Reference: https://developers.podio.com/doc/space-members/get-space-membership-22397 </para>
+        /// </summary>
+        /// <param name="spaceId"></param>
+        /// <param name="userId"></param>
+        /// <returns></returns>
+        public SpaceMember GetSpaceMembership(int spaceId, int userId)
+        {
+            string url = string.Format("/space/{0}/member/{1}", spaceId, userId);
+            return _podio.Get<SpaceMember>(url);
+        }
+
+        /// <summary>
+        /// Joins the open space with the given id.
+        /// <para>Podio API Reference: https://developers.podio.com/doc/space-members/join-space-1927286 </para>
+        /// </summary>
+        /// <param name="spaceId"></param>
+        public void JoinSpace(int spaceId)
+        {
+            string url = string.Format("/space/{0}/join", spaceId);
+            _podio.Post<dynamic>(url);
+        }
+
+        /// <summary>
+        /// Used to leave the space for the active user.
+        /// <para>Podio API Reference: https://developers.podio.com/doc/space-members/leave-space-19410457 </para>
+        /// </summary>
+        /// <param name="spaceId"></param>
+        public void LeaveSpace(int spaceId)
+        {
+            string url = string.Format("/space/{0}/leave", spaceId);
+            _podio.Post<dynamic>(url);
+        }
+
+        /// <summary>
+        /// Updates the space memberships with another role.
+        /// <para>Podio API Reference: https://developers.podio.com/doc/space-members/update-space-memberships-22398 </para>
+        /// </summary>
+        /// <param name="spaceId"></param>
+        /// <param name="userIds"></param>
+        /// <param name="role"></param>
+        public void UpdateSpaceMemberships(int spaceId, int[] userIds, string role)
+        {
+            string userIdsCSV = Utilities.ArrayToCSV(userIds);
+            string url = string.Format("/space/{0}/member/{1}", spaceId, userIdsCSV);
+            dynamic requestData = new
+            {
+                role = role
+            };
+            _podio.Put<dynamic>(url, requestData);
+        }
+
+        /// <summary>
+        /// Request access to a space you don't have access to. All admins of this space will get notified and can accept or ignore it.
+        /// <para>Podio API Reference: https://developers.podio.com/doc/space-members/request-space-membership-6146231 </para>
+        /// </summary>
+        /// <param name="spaceId"></param>
+        public void RequestSpaceMembership(int spaceId)
+        {
+            string url = string.Format("/space/{0}/member_request/", spaceId);
+            _podio.Post<dynamic>(url);
         }
     }
 }
