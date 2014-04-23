6945920df13ac54eedca030d317ed8a410f46e77
Ajmal Vh <avh@phases.dk>
2014-04-23 16:14:11 +0530

    added reference models

---
+++ Podio .NET/Models/ReferenceGroup.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace PodioAPI.Models
+{
+    public class ReferenceGroup
+    {
+        /// <summary>
+        ///  The name of the group; one of {"spaces", "app", "profiles", "created_bys", "created_bys", "tags", "space_contacts", "space_members", "auth_clients", "tasks"}
+        /// </summary>
+        [JsonProperty(PropertyName = "name")]
+        public string Name { get; set; }
+
+        [JsonProperty(PropertyName = "data")]
+        public JObject Data { get; set; }
+
+        [JsonProperty(PropertyName = "contents")]
+        public JArray Contents { get; set; }
+    }
+}
