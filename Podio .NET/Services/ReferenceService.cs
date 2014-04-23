4e144137b2539ab60b2404ca1e328af9a5f11d4e
Ajmal Vh <avh@phases.dk>
2014-04-23 16:24:08 +0530

    Added SearchReferences method

--- Podio .NET/Services/ReferenceService.cs
+++ Podio .NET/Services/ReferenceService.cs
@@ -36,7 +36,7 @@ namespace PodioAPI.Services
         /// <param name="limit">The maximum number of profiles to return for each access type</param>
         /// <param name="offset">The offset into the profiles to return</param>
         /// <returns></returns>
-        public List<Contact> FindUserWithAccess(string refType, int refId,int? limit = null, int? offset = null)
+        public List<Contact> FindUserWithAccess(string refType, int refId, int? limit = null, int? offset = null)
         {
             string url = string.Format("/reference/{0}/{1}/accessible_by/", refType, refId);
             var requestData = new Dictionary<string, string>()
@@ -57,7 +57,7 @@ namespace PodioAPI.Services
         /// <param name="contextId">The id of the context to check the reference against.</param>
         /// <param name="contextType">The context type to check the reference against. Currently only supports "app".</param>
         /// <returns></returns>
-        public Reference GetReference(string refType, int refId, bool accessorCount = false, int? contextId = null, string contextType = null) 
+        public Reference GetReference(string refType, int refId, bool accessorCount = false, int? contextId = null, string contextType = null)
         {
             string url = string.Format("/reference/{0}/{1}", refType, refId);
             var requestData = new Dictionary<string, string>()
@@ -85,5 +85,16 @@ namespace PodioAPI.Services
             return _podio.Get<Reference>(url, requestData);
         }
 
+        /// <summary>
+        /// Search for references for use in various contexts.
+        /// <para>Podio API Reference: https://developers.podio.com/doc/reference/search-references-13312595 </para>
+        /// </summary>
+        /// <param name="searchReferenceRequest"></param>
+        /// <returns></returns>
+        public List<ReferenceGroup> SearchReferences(SearchReferencesRequest searchReferenceRequest)
+        {
+            string url = "/reference/search";
+            return _podio.Post<List<ReferenceGroup>>(url, searchReferenceRequest);
+        }
     }
 }
