fccb8f8fcaa416eb220eb73ade269a26aefe93aa
Ajmal Vh <avh@phases.dk>
2014-04-28 18:58:16 +0530

    Search response model

---
+++ Podio .NET/Models/SearchResult.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace PodioAPI.Models
+{
+    public class SearchResult
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("type")]
+        public string Type { get; set; }
+
+        [JsonProperty("rank")]
+        public int Rank { get; set; }
+
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+        [JsonProperty("created_on")]
+        public DateTime CreatedOn { get; set; }
+
+        [JsonProperty("link")]
+        public string Link { get; set; }
+
+        [JsonProperty("app")]
+        public Application App { get; set; }
+
+        [JsonProperty("org")]
+        public OrganizationMicro Org { get; set; }
+
+        [JsonProperty("space")]
+        public SpaceMicro Space { get; set; }
+
+        [JsonProperty("created_by")]
+        public ByLine CreatedBy { get; set; }
+
+    }
+}
