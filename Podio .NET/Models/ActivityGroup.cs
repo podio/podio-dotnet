61da7eaaa0359061e1847ed66ac10bf3ad743db9
Ajmal Vh <avh@phases.dk>
2014-05-01 11:56:17 +0530

    StreamV3 models

---
+++ Podio .NET/Models/ActivityGroup.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+namespace PodioAPI.Models
+{
+    public class ActivityGroup
+    {
+        [JsonProperty("activities")]
+        public List<Activity> Activities { get; set; }
+
+        [JsonProperty("authors")]
+        public ByLine Authors { get; set; }
+
+        [JsonProperty("created_by")]
+        public ByLine CreatedBy { get; set; }
+
+        [JsonProperty("created_on")]
+        public DateTime CreatedOn { get; set; }
+
+        [JsonProperty("created_via")]
+        public Via CreatedVia { get; set; }
+
+        [JsonProperty("group_id")]
+        public int GroupId { get; set; }
+
+        [JsonProperty("kind")]
+        public string Kind { get; set; }
+    }
+}
---
+++ Podio .NET/Models/StreamObjectV3.cs
@@ -0,0 +1,61 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+
+namespace PodioAPI.Models
+{
+    public class StreamObjectV3
+    {
+        [JsonProperty("activity_groups")]
+        public List<ActivityGroup> ActivityGoups { get; set; }
+
+        [JsonProperty("app")]
+        public Application App { get; set; }
+
+        [JsonProperty("created_by")]
+        public ByLine CreatedBy { get; set; }
+
+        [JsonProperty("created_on")]
+        public DateTime CreatedOn { get; set; }
+
+        [JsonProperty("data")]
+        public JObject Data { get; set; }
+
+        [JsonProperty("files")]
+        public List<FileAttachment> Files { get; set; }
+
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("is_liked")]
+        public bool IsLiked { get; set; }
+
+        [JsonProperty("like_count")]
+        public int LikeCount { get; set; }
+
+        [JsonProperty("link")]
+        public string link { get; set; }
+
+        [JsonProperty("org")]
+        public Organization Org { get; set; }
+
+        [JsonProperty("rights")]
+        public string[] Rights { get; private set; }
+
+        [JsonProperty("space")]
+        public SpaceMicro Space { get; set; }
+
+        [JsonProperty("subscribed")]
+        public bool Subscribed { get; set; }
+
+        [JsonProperty("subscribed_count")]
+        public int SubscribedCount { get; set; }
+
+        [JsonProperty("title")]
+        public string title { get; set; }
+
+        [JsonProperty("type")]
+        public string Type { get; set; }
+    }
+}
