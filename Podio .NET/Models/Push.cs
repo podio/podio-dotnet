e73506bd16cb30c733f231da6b4a7e16b5b6064e
Ajmal Vh <avh@phases.dk>
2014-05-05 17:35:48 +0530

    Added model for push

---
+++ Podio .NET/Models/Push.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace PodioAPI.Models
+{
+    public class Push
+    {
+        [JsonProperty("timestamp")]
+        public long Timestamp { get; set; }
+
+        [JsonProperty("expires_in")]
+        public long ExpiresIn { get; set; }
+
+        [JsonProperty("channel")]
+        public string Channel { get; set; }
+
+        [JsonProperty("signature")]
+        public string Signature { get; set; }
+    }
+}
