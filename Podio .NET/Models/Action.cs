eb5094f91681744bc4af3f69c34609b900b5f86e
Ajmal Vh <avh@phases.dk>
2014-05-06 12:07:04 +0530

    Updated Action Model

--- Podio .NET/Models/Action.cs
+++ Podio .NET/Models/Action.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Newtonsoft.Json;
 
 namespace PodioAPI.Models
@@ -12,10 +13,45 @@ namespace PodioAPI.Models
         public string Type { get; set; }
 
         [JsonProperty("data")]
-        public Dictionary<string,object> Data { get; set; }
+        public Dictionary<string, object> Data { get; set; }
 
         [JsonProperty("text")]
         public string Text { get; set; }
 
+        [JsonProperty("comments")]
+        public List<Comment> Comments { get; set; }
+
+        [JsonProperty("created_by")]
+        public ByLine CreatedBy { get; set; }
+
+        [JsonProperty("created_on")]
+        public DateTime? CreatedOn { get; set; }
+
+        [JsonProperty("created_via")]
+        public Via CreatedVia { get; set; }
+
+        [JsonProperty("is_liked")]
+        public bool IsLiked { get; set; }
+
+        [JsonProperty("like_count")]
+        public bool LikeCount { get; set; }
+
+        [JsonProperty("pinned")]
+        public bool Pineed { get; set; }
+
+        [JsonProperty("presence")]
+        public Presence Presence { get; set; }
+
+        [JsonProperty("push")]
+        public Push Push { get; set; }
+
+        [JsonProperty("rights")]
+        public string[] Rights { get; set; }
+
+        [JsonProperty("subscribed")]
+        public bool Subscribed { get; set; }
+
+        [JsonProperty("subscribed_count")]
+        public int SubscribedCount { get; set; }
     }
 }
