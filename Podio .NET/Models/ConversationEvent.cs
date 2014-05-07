4395f2ba08fd53ecf32a761247507f3078f40599
Ajmal Vh <avh@phases.dk>
2014-05-07 18:58:04 +0530

    New conversation related models

---
+++ Podio .NET/Models/ConversationEvent.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+
+namespace PodioAPI.Models
+{
+    public class ConversationEvent
+    {
+        [JsonProperty("event_id")]
+        public int EventId { get; set; }
+
+        [JsonProperty("created_on")]
+        public DateTime? CreatedOn { get; set; }
+
+        [JsonProperty("created_by")]
+        public ByLine CreatedBy { get; set; }
+
+        [JsonProperty("created_via")]
+        public Via CreatedVia { get; set; }
+
+        [JsonProperty("action")]
+        public string Action { get; set; }
+
+        [JsonProperty("data")] 
+        public JObject Data { get; set; }
+    }
+}
---
+++ Podio .NET/Models/Request/ConversationCreateRequest.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace PodioAPI.Models.Request
+{
+    public class ConversationCreateRequest
+    {
+        [JsonProperty("subject", NullValueHandling = NullValueHandling.Ignore)]
+        public string Subject { get; set; }
+
+        [JsonProperty("text", NullValueHandling = NullValueHandling.Ignore)]
+        public string Text { get; set; }
+
+        [JsonProperty("file_ids", NullValueHandling = NullValueHandling.Ignore)]
+        public List<int> FileIds { get; set; }
+
+        [JsonProperty("participants", NullValueHandling = NullValueHandling.Ignore)]
+        public List<int> Participants { get; set; }
+
+        [JsonProperty("embed_id", NullValueHandling = NullValueHandling.Ignore)]    
+        public int? EmbedId { get; set; }
+
+        [JsonProperty("embed_url", NullValueHandling = NullValueHandling.Ignore)]    
+        public string EmbedUrl { get; set; }
+    }
+}
