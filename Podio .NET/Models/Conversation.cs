273d561930dff59bee90ba8af8d9af0183a74226
Ajmal Vh <avh@phases.dk>
2014-05-07 18:57:44 +0530

    Updated conversation model

--- Podio .NET/Models/Conversation.cs
+++ Podio .NET/Models/Conversation.cs
@@ -6,17 +6,47 @@ namespace PodioAPI.Models
 {
     public class Conversation
     {
-        [JsonProperty(PropertyName = "conversation_id")]
-        public int? ConversationId { get; set; }
+        [JsonProperty(PropertyName = "unread_count")]
+        public int? UnreadCount { get; set; }
 
-        [JsonProperty(PropertyName = "subject")]
-        public string Subject { get; set; }
+        [JsonProperty(PropertyName = "presence")]
+        public Presence Presence { get; set; }
+
+        [JsonProperty(PropertyName = "participants", Required = Required.AllowNull)]
+        public List<User> Participants { get; set; }
+
+        [JsonProperty(PropertyName = "excerpt")]
+        public string Excerpt { get; set; }
+
+        [JsonProperty(PropertyName = "created_by")]
+        public ByLine CreatedBy { get; set; }
+
+        [JsonProperty(PropertyName = "push")]
+        public Push Push { get; set; }
 
         [JsonProperty(PropertyName = "created_on")]
         public DateTime CreatedOn { get; set; }
 
+        [JsonProperty(PropertyName = "last_event_on")]
+        public DateTime LastEventOn { get; set; }
+
+        [JsonProperty(PropertyName = "conversation_id")]
+        public int? ConversationId { get; set; }
+
+        [JsonProperty(PropertyName = "starred")]
+        public bool Starred { get; set; }
+
+        [JsonProperty(PropertyName = "unread")]
+        public bool Unread { get; set; }
+
+        [JsonProperty(PropertyName = "type")]
+        public string Type { get; set; }
+
+        [JsonProperty(PropertyName = "subject")]
+        public string Subject { get; set; }
+
         [JsonProperty(PropertyName = "ref")]
-        public Reference Ref { get; set; }
+        public Ref Ref { get; set; }
 
         [JsonProperty(PropertyName = "embed")]
         public Embed Embed { get; set; }
@@ -24,16 +54,13 @@ namespace PodioAPI.Models
         [JsonProperty(PropertyName = "embed_file")]
         public FileAttachment EmbedFile { get; set; }
 
-        [JsonProperty(PropertyName = "created_by")]
-        public ByLine CreatedBy { get; set; }
-
         [JsonProperty(PropertyName = "files")]
         public List<FileAttachment> Files { get; set; }
 
         [JsonProperty(PropertyName = "messages")]
         public List<ConversationMessage> Messages { get; set; }
 
-        [JsonProperty(PropertyName = "participants_full", Required = Required.AllowNull)]
-        public List<ConversationParticipant> ParticipantsFull { get; set; }
+        
     }
+   
 }
