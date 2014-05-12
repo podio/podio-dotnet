d3fe5a024db8d062e2e53142085adfa51fc4f55c
Ajmal Vh <avh@phases.dk>
2014-05-12 11:10:06 +0530

    Added ImportedInfo Model, for importer service

---
+++ Podio .NET/Models/ImporterInfo.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace PodioAPI.Models
+{
+    public class ImporterInfo
+    {
+        [JsonProperty("row_count")]
+        public int RowCount { get; set; }
+
+        [JsonProperty("columns")]
+        List<FileColumn> Columns { get; set; }
+    }
+    public class FileColumn
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+    }
+}
