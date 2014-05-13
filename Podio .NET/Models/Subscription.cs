3bfe9c927aa9a531d2468412fa5924c26b0964cb
Ajmal Vh <avh@phases.dk>
2014-05-13 12:35:31 +0530

    Subscription model

---
+++ Podio .NET/Models/Subscription.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace PodioAPI.Models
+{
+    public class Subscription
+    {
+        [JsonProperty("started_on")]
+        public DateTime StartedOn { get; set; }
+
+        [JsonProperty("notifications")]
+        public int Notifications { get; set; }
+
+        [JsonProperty("ref")]
+        public Ref Reference { get; set; }
+    }
+}
