18ffee7c670ef617311fcda9deff00bdca118551
Ajmal Vh <avh@phases.dk>
2014-05-15 14:19:36 +0530

    Integration are request model

---
+++ Podio .NET/Models/IntegrationAvailableAppField.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace PodioAPI.Models
+{
+    public class IntegrationAvailableAppField
+    {
+        [JsonProperty("external_id")]
+        public string ExternalId { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("type")]
+        public string Type { get; set; }
+    }
+}
