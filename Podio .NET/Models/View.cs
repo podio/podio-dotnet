8b1ef0bfa625722d356a33c45fdd30ff6705cf01
Ajmal Vh <avh@phases.dk>
2014-05-15 11:25:03 +0530

    Added view model

---
+++ Podio .NET/Models/View.cs
@@ -0,0 +1,48 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+
+namespace PodioAPI.Models
+{
+    public class View
+    {
+        [JsonProperty("view_id")]
+        public int viewId { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("created_on")]
+        public DateTime CreatedOn { get; set; }
+
+        [JsonProperty("sort_by")]
+        public string SortBy { get; set; }
+
+        [JsonProperty("sort_desc")]
+        public string SortDesc { get; set; }
+
+        [JsonProperty("filters")]
+        public JArray Filters { get; set; }
+
+        [JsonProperty("fields")]
+        public JObject Fields { get; set; }
+
+        [JsonProperty("created_by")]
+        public ByLine CreatedBy { get; set; }
+
+        [JsonProperty("layout")]
+        public string Layout { get; set; }
+
+        [JsonProperty("type")]
+        public string Type { get; set; }
+
+        [JsonProperty("rights")]
+        public string[] Rights { get; set; }
+
+        [JsonProperty("filter_id")]
+        public int FilterId { get; set; }
+
+        [JsonProperty("items")]
+        public int Items { get; set; }
+    }
+}
