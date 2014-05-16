72e573d81a82343d7b58ed763ce67cdc02fd9690
Ajmal Vh <avh@phases.dk>
2014-05-16 18:53:07 +0530

    Add Cause model for Flows area, Updated dll version to 9.6.0

---
+++ Podio .NET/Models/Cause.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PodioAPI.Models
+{
+    public class Cause
+    {
+        [JsonProperty("type", NullValueHandling = NullValueHandling.Ignore)]
+        public string Type { get; set; }
+
+        [JsonProperty("config", NullValueHandling = NullValueHandling.Ignore)]
+        public dynamic Config { get; set; }
+    }
+}
