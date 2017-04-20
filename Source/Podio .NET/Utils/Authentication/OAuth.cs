01c358a61221202f1145a16d98929940fd62f3e5
CITRITE\sagart <sagar.thirumala@citrix.com>
2017-04-20 00:09:39 -0400

    First cut - add support for SF access token based auth Remove validations from AuthenticateWithShareFile Refactor OAuth, add new base abstract class OAuth Some more refactoring and make the ApiUrl overridable

---
+++ Source/Podio .NET/Utils/Authentication/OAuth.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+using System.Net.Http.Headers;
+
+namespace PodioAPI.Utils.Authentication
+{
+    public abstract class OAuth
+    {
+        [JsonProperty(PropertyName = "access_token")]
+        public string AccessToken { get; set; }
+
+        internal abstract bool IsAuthenticated();
+        internal abstract void AddAuthorizationHeader(HttpRequestHeaders headers);
+    }
+}
